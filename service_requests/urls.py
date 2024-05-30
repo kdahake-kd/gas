506cb53eb790d713ddff8106df2e43f3928d4d2e
kiran-kd <kdahake412@gmail.com>
2024-05-30 09:40:57 +0530

    comment added for testing purpose

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -1,4 +1,4 @@
-
+#url added 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ServiceRequestViewSet
--- service_requests/views.py
+++ service_requests/views.py
@@ -1,3 +1,4 @@
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import ServiceRequest
