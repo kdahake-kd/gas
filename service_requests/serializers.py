1cfe8c11dadf93701cfa1cf8e1c80b5ec1d9a305
kiran-kd <kdahake412@gmail.com>
2024-05-30 09:37:09 +0530

    update the view for allow the unauthenticate user to register the service

--- service_requests/serializers.py
+++ service_requests/serializers.py
@@ -1,10 +1,12 @@
-
-
 from rest_framework import serializers
 from .models import ServiceRequest
+from django.contrib.auth.models import User
 
 class ServiceRequestSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+
     class Meta:
         model = ServiceRequest
         fields = '__all__'
         read_only_fields = ['status', 'created_at', 'updated_at']
+
--- service_requests/views.py
+++ service_requests/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import ServiceRequest
 from .serializers import ServiceRequestSerializer
+from django.contrib.auth.models import User
+
 
 class ServiceRequestViewSet(viewsets.ModelViewSet):
     queryset = ServiceRequest.objects.all()
@@ -14,8 +16,10 @@ class ServiceRequestViewSet(viewsets.ModelViewSet):
         return ServiceRequest.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-
-
+        user = self.request.data.get('user')
+        if user:
+            user = User.objects.get(pk=user)
+            serializer.save(user=user)
+        else:
+            serializer.save(user=self.request.user)
 
